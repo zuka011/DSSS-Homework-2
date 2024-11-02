c52939b981a8b3a0c2afd05dc3e1f8cd39f852a8
Zurab Mujirishvili <zurab.mujirishvili@fau.de>
2024-11-02 13:39:59 +0100

    Add license, README and installation script

---
+++ README.md
@@ -0,0 +1,27 @@
+# DSSS Homework 2
+
+This is a miniature git repository that contains a math quiz for basic arithmetic operations.
+
+## Running the Quiz
+
+To run the quiz, you can simply install the package with the following command:
+
+```bash
+pip install git+https://github.com/zuka011/DSSS-Homework-2.git
+```
+
+Afterwards, you can run the quiz with the following command:
+
+```bash
+math_quiz
+```
+
+## Running the Tests
+
+To run the tests, you can pull the repository and run the following command:
+
+```bash
+pytest
+```
+
+That's it! Enjoy the quiz!
---
+++ setup.py
@@ -0,0 +1,25 @@
+# This is a very old way of configuring python packages.
+# Modern python codebases use a proper dependency manager like poetry.
+from setuptools import setup, find_packages
+
+# Read requirements.txt
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=required,
+    author="Zurab Mujirishvili",
+    author_email="zurab.mujirishvili@fau.de",
+    description="A simple math quiz game",
+    keywords="math, quiz, game",
+    url="https://github.com/zuka011/DSSS-Homework-2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
